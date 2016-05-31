31676416a61ee4463575402a267a8f00c79838ea
Markus Endres <markus.endres@realtream.com>
2016-05-31 13:13:08 +0200

    Code Optimization And Cleaning

--- BinaryChop/BinaryChop/BinaryChopAlgorithm1.cs
+++ BinaryChop/BinaryChop/BinaryChopAlgorithm1.cs
@@ -26,7 +26,7 @@ namespace BinaryChop
         {
             int remainingItems = valuesToChop.Count();
             int middlePositionValue = GetMiddleValue(valuesToChop);
-            int[] newValuesToChop = new int[0];
+            int[] newValuesToChop;
 
             chopedRight = 0;
             if (ValueGreaterThen(middlePositionValue, valueToSearch, valuesToChop))
@@ -36,23 +36,35 @@ namespace BinaryChop
                 {
                     chopedRight--;
                 }
-                newValuesToChop = new int[middlePositionValue];
-                int o = 0;
-                for (int i = 0; i < middlePositionValue; i++)
-                {
-                    newValuesToChop[o] = valuesToChop[i];
-                    o++;
-                }
+                newValuesToChop = RemoveItemsFromTheRightSide(valuesToChop, middlePositionValue);
             }
             else
             {
-                newValuesToChop = new int[remainingItems - middlePositionValue];
-                int o = 0;
-                for (int i = middlePositionValue; i < remainingItems; i++)
-                {
-                    newValuesToChop[o] = valuesToChop[i];
-                    o++;
-                }
+                newValuesToChop = RemoveItemsFromTheLeftSide(valuesToChop, remainingItems, middlePositionValue);
+            }
+            return newValuesToChop;
+        }
+
+        private static int[] RemoveItemsFromTheLeftSide(int[] valuesToChop, int remainingItems, int middlePositionValue)
+        {
+            var newValuesToChop = new int[remainingItems - middlePositionValue];
+            int o = 0;
+            for (int i = middlePositionValue; i < remainingItems; i++)
+            {
+                newValuesToChop[o] = valuesToChop[i];
+                o++;
+            }
+            return newValuesToChop;
+        }
+
+        private static int[] RemoveItemsFromTheRightSide(int[] valuesToChop, int middlePositionValue)
+        {
+            var newValuesToChop = new int[middlePositionValue];
+            int o = 0;
+            for (int i = 0; i < middlePositionValue; i++)
+            {
+                newValuesToChop[o] = valuesToChop[i];
+                o++;
             }
             return newValuesToChop;
         }
