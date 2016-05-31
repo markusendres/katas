ef218a56189410d5cd9e114620850c50b9d9ee79
Markus Endres <markus.endres@realtream.com>
2016-05-31 14:06:21 +0200

    Program.cs

--- BinaryChop/BinaryChop/Program.cs
+++ BinaryChop/BinaryChop/Program.cs
@@ -12,9 +12,11 @@ namespace BinaryChop
         {
 
             var algorithm1 = new BinaryChopAlgorithm1();
-            int result = algorithm1.FindPositionOfValueInSortedNumberArray(6, new[] { 1, 2, 5, 6, 7});
+            int result = algorithm1.FindPositionOfValueInSortedNumberArray(6, new[] { 1, 2, 5, 6, 7, 8, 9}) - 1;
+            int result2 = Array.IndexOf(new int[] {1, 2, 5, 6, 7, 8, 9 }, 6);
 
             Console.WriteLine(result);
+            Console.WriteLine(result2);
             Console.ReadLine();
         }
     }
